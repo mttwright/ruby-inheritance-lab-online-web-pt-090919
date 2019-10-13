4a3435bdab35bf0cf2de86611c77ed689b456fef
virtual-frame-7789 <mttwright@gmail.com>
2019-10-13 21:29:28 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,13 @@
 class User
   
+  attr_reader :first_name, :last_name
   
+  def first_name=(first)
+    @first_name = first
+  end
+  
+  def last_name=(last)
+    @last_name = last
+  end
   
 end
